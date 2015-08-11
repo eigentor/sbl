ec74d2cf18de6bf8f54032978065ddd51c8a62a8
marc.lang <marc.lang@gobsy.de>
2015-08-11 15:05:01 +0200

    Inhalte und Views

--- sites/all/themes/seven_sbl/js/sbl_custom.js
+++ sites/all/themes/seven_sbl/js/sbl_custom.js
@@ -11,7 +11,6 @@ jQuery(document).ready(
     function()
     {
         jQuery("#article-node-form").submit(update_field);
-
         function update_field()
         {
 		var pgn_location=jQuery('a[href$=".pgn"]:first').attr('href');
@@ -19,7 +18,23 @@ jQuery(document).ready(
 		var pgn='[pgn pgnData=' + pgn_location + ']<a href=' + pgn_location + '>PGN</a>[/pgn]';
 		jQuery("#edit-field-pgn-implementierung-und-0-value").val(pgn);
 		}
-		else jQuery("#edit-field-pgn-implementierung-und-0-value").val("");
+		else {
+			jQuery("#edit-field-pgn-implementierung-und-0-value").val("");
+			}
+        };
+		
+		jQuery("#partiendownload-node-form").submit(update_field2);
+        function update_field2()
+        {
+		var pgn_location2=jQuery('a[href$=".pgn"]:first').attr('href');
+		if(pgn_location2 && pgn_location2.length > 5) {
+		var pgn2='[pgn pgnData=' + pgn_location2 + ']<a href=' + pgn_location2 + '>PGN</a>[/pgn]';
+		jQuery("#edit-field-gesamt-pgn-implementierung-und-0-value").val(pgn2);
+		}
+		else {
+		jQuery("#edit-field-gesamt-pgn-implementierung-und-0-value").val("");
+		}
         }
     }
+
 );
