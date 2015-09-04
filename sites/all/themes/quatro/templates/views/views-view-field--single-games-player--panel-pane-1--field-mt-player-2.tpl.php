fa93b43e8045b5671797659ae27a811623fe1044
Thomas Moseler <tmoseler@gmx.de>
2015-09-04 12:59:53 +0200

    Gegnername in Einzelspielen pro Spieler immer richtig anzeigen

---
+++ sites/all/themes/quatro/templates/views/views-view-field--single-games-player--panel-pane-1--field-mt-player-2.tpl.php
@@ -0,0 +1,12 @@
+<?php
+  dpm($row);
+  // If the player, whose page is displayed, is player 2, display player 1 in the
+  // Column "Gegner: Name"
+  $player = arg(1);
+  if($row->field_field_mt_player_2[0]['raw']['target_id'] == $player) {
+    $output = $row->field_field_mt_player_1[0]['rendered']['#markup'];
+  }
+?>
+
+<?php print $output; ?>
+
