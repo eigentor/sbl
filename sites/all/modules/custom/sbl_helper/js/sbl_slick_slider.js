fc959037330bdff4f5ffb257fbc20c7f87e5cdb6
Thomas Moseler <tmoseler@gmx.de>
2015-09-07 19:33:33 +0200

    Active-Klasse und Element für Pfeil für Thumbs Slideshow

--- sites/all/modules/custom/sbl_helper/js/sbl_slick_slider.js
+++ sites/all/modules/custom/sbl_helper/js/sbl_slick_slider.js
@@ -35,6 +35,44 @@ $('.front .view-article-slider-large.view-display-id-block_1 .view-content').sli
     focusOnSelect: true
 });
 
+// Set an active class for the active thumbnail
+// Get the initial active slide on page load
+var active_slide = $('.front .view-article-slider-large.view-display-id-block .views-row.slick-active').index();
+var number_thumb = $('.front .view-article-slider-large.view-display-id-block_1 .views-row').get(active_slide);
+$(number_thumb).addClass('active-thumb');
+$(number_thumb).siblings().removeClass('active-thumb');
+$(number_thumb).prepend('<span class="active-arrow"></span>');
+$(number_thumb).siblings().find('span.active-arrow').remove();
+
+
+
+// We need to call it as Slick event
+// On after slide change
+$('.front .view-article-slider-large.view-display-id-block .view-content').on('beforeChange', function(event, slick, currentSlide, nextSlide) {
+    active_slide = $('.front .view-article-slider-large.view-display-id-block .views-row.slick-active').index();
+    if(active_slide == 3) {
+        active_slide = 0;
+    } else {
+        console.log(active_slide);
+        active_slide = active_slide + 1;
+    }
+
+    var number_thumb = $('.front .view-article-slider-large.view-display-id-block_1 .views-row').get(active_slide);
+    $(number_thumb).addClass('active-thumb');
+    $(number_thumb).siblings().removeClass('active-thumb');
+    $(number_thumb).prepend('<span class="active-arrow"></span>');
+    $(number_thumb).siblings().find('span.active-arrow').remove();
+
+});
+
+$('.front .view-article-slider-large.view-display-id-block_1 .views-row').click(function(){
+    $(this).addClass('active-thumb');
+    $(this).siblings().removeClass('active-thumb');
+    $(this).prepend('<span class="active-arrow"></span>');
+    $(this).siblings().find('span.active-arrow').remove();
+});
+
+
 
 
 
