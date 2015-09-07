c5fd150319418afe53f57b122c5ce7f894d8318b
Thomas Moseler <tmoseler@gmx.de>
2015-09-07 12:40:22 +0200

    Slideshow Startseite ein Anfang ist gemacht

---
+++ sites/all/modules/custom/sbl_helper/js/sbl_slick_slider.js
@@ -0,0 +1,27 @@
+(function ($) {
+
+  // Scripte als Drupal Behaviors hinzufuegen statt als document.ready.
+  // Funktioniert dann auch bei Ajax Reloads.
+  Drupal.behaviors.sbl_helper = {
+    attach: function (context, settings) {
+
+//Die Header Slideshow auf der Startseite
+$('.front .view-article-slider-large.view-display-id-block .view-content').slick({
+    dots: true,
+    arrows: false,
+    infinite: true,
+    autoplay: true,
+    autoplaySpeed: 4000,
+    fade: true,
+    speed: 700,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    slide: 'div.views-row',
+});
+
+
+
+
+    } // end of attach function
+  };
+})(jq111); // This makes sure everything above uses Jquery 1.11
