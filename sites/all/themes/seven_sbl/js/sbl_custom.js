2c6d324359f7fa708d71ebec64b0d580e3fe1dbd
marc.lang <marc.lang@gobsy.de>
2015-09-29 12:12:26 +0200

    Neuer PGN Viewer

--- sites/all/themes/seven_sbl/js/sbl_custom.js
+++ sites/all/themes/seven_sbl/js/sbl_custom.js
@@ -15,7 +15,7 @@ jQuery(document).ready(
         {
 		var pgn_location=jQuery('a[href$=".pgn"]:first').attr('href');
 		if(pgn_location && pgn_location.length > 5) {
-		var pgn='[pgn pgnData=' + pgn_location + ']<a href=' + pgn_location + '>PGN</a>[/pgn]';
+		var pgn='[canvas]' + pgn_location + '[/canvas]';
 		jQuery("#edit-field-pgn-implementierung-und-0-value").val(pgn);
 		}
 		else {
