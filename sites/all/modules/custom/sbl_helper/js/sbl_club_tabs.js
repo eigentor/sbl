290a10ad865e63481f282219f2a5b9e31a095165
Thomas Moseler <tmoseler@gmx.de>
2016-01-20 22:13:34 +0100

    View Ergebnisse pro Club verbessert

--- sites/all/modules/custom/sbl_helper/js/sbl_club_tabs.js
+++ sites/all/modules/custom/sbl_helper/js/sbl_club_tabs.js
@@ -4,12 +4,8 @@
   Drupal.behaviors.sbl_helper_form_submit = {
     attach: function (context, settings) {
       $('#teamseite #tabs-0-bottom li.first a').text('Kader');
-      // Set the board number for the results of each team
+      $('#teamseite #tabs-0-bottom li.last a').text('Ergebnisse');
 
-      $('.view-match-results.view-display-id-block_1 table tr').each(function() {
-        count = $(this).index() + 1;
-       $(this).find('td.views-field-counter').text(count);
-      });
     } // end of attach function
   };
 })(jQuery);
