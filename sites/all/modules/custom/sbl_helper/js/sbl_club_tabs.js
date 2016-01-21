a7ab370b33c23a859372f4b3344e585e0f8767de
Thomas Moseler <tmoseler@gmx.de>
2016-01-21 10:17:33 +0100

    Scrollposition und ausgewählten Tab beibehalten bei Klick auf den Pager in Ergebnisse pro Mannschaft

--- sites/all/modules/custom/sbl_helper/js/sbl_club_tabs.js
+++ sites/all/modules/custom/sbl_helper/js/sbl_club_tabs.js
@@ -6,6 +6,17 @@
       $('#teamseite #tabs-0-bottom li.first a').text('Kader');
       $('#teamseite #tabs-0-bottom li.last a').text('Ergebnisse');
 
+      // Die Scrollposition beibehalten, wenn man auf den Pager klickt
+      var elementToTest = $("body.page-verein");
+      if(elementToTest.length > 0) {
+        $(window).scrollTop(window.name);
+        window.name = 0;
+      }
+
+      $('#teamseite .results-pager a').click(function(){
+        window.name = $(window).scrollTop();
+      });
+
     } // end of attach function
   };
 })(jQuery);
