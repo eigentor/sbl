d0a20434a0b62678abe080c5ea69f6aaf2d07929
marc.lang <marc.lang@gobsy.de>
2016-01-31 23:35:25 +0100

    JS

--- xml/format_xml.js
+++ xml/format_xml.js
@@ -1,23 +1,17 @@
-<script type="text/javascript">
-var xmlDoc;
-var xmlFile="aktuell.xml";
-
-if(typeof window.DOMParser != "undefined") {
-    xmlhttp=new XMLHttpRequest();
-    xmlhttp.open("GET",xmlFile,false);
-    if (xmlhttp.overrideMimeType){
-        xmlhttp.overrideMimeType('text/xml');
-    }
-    xmlhttp.send();
-    xmlDoc=xmlhttp.responseXML;
-}
-else{
-    xmlDoc = new ActiveXObject("Microsoft.XMLDOM");
-    xmlDoc.async="false";
-    xmlDoc.load(xmlFile);
-}
-var tagObj=xmlDoc.getElementsByTagName("item");
-var titleValue = tagObj[0].getElementsByTagName("title")[0].childNodes[0].nodeValue;
-var descriptionValue = tagObj[0].getElementsByTagName("description")[0].childNodes[0].nodeValue;
-alert(titleValue);
-</script>
+<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+    <script type="text/javascript">
+    $(document).ready(function () {
+        $.ajax({
+            type: "GET",
+            url: "../aktuell.xml",
+            cache: false,
+            dataType: "xml",
+            success: function(xml) {
+                $(xml).find('items').each(function(){
+                    var name = $(this).find("title").text()
+                    alert(name);
+                });
+            }
+        });
+    });
+	</script>
