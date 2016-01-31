b0cec49058cd31782e925c998c7de36c83f60f1b
marc.lang <marc.lang@gobsy.de>
2016-01-31 01:16:57 +0100

    XML

--- xml/format_xml.js
+++ xml/format_xml.js
@@ -1,34 +1,21 @@
-function include(filename, onload) {
-    var head = document.getElementsByTagName('head')[0];
-    var script = document.createElement('script');
-    script.src = filename;
-    script.type = 'text/javascript';
-    script.onload = script.onreadystatechange = function() {
-        if (script.readyState) {
-            if (script.readyState === 'complete' || script.readyState === 'loaded') {
-                script.onreadystatechange = null;                                                  
-                onload();
-            }
-        } 
-        else {
-            onload();          
-        }
-    };
-    head.appendChild(script);
-}
+var xmlDoc;
+var xmlFile="aktuell.xml";
 
-include('http://ajax.googleapis.com/ajax/libs/jquery/1.7.0/jquery.min.js', function() {
-    $(document).ready(function() {
-        $.get("aktuell.xml",{},function(xml){
-  $('item',xml).each(function(i) {
-	  var title = $(this).find("title").text();
-	  var description = $(this).find("description").text();
-	  var output = '<div style="border:1px solid #c0c0c0; margin:10px;">';	
-		output += '<b>'+ title + '</b><br />';
-		output += '<u>'+ description + '<br />';	
-		output += '</div>';
-		document.write(output):
-  }
- }
-    });
-});
+if(typeof window.DOMParser != "undefined") {
+    xmlhttp=new XMLHttpRequest();
+    xmlhttp.open("GET",xmlFile,false);
+    if (xmlhttp.overrideMimeType){
+        xmlhttp.overrideMimeType('text/xml');
+    }
+    xmlhttp.send();
+    xmlDoc=xmlhttp.responseXML;
+}
+else{
+    xmlDoc = new ActiveXObject("Microsoft.XMLDOM");
+    xmlDoc.async="false";
+    xmlDoc.load(xmlFile);
+}
+var tagObj=xmlDoc.getElementsByTagName("item");
+var titleValue = tagObj[0].getElementsByTagName("title")[0].childNodes[0].nodeValue;
+var descriptionValue = tagObj[0].getElementsByTagName("description")[0].childNodes[0].nodeValue;
+alert(titleValue);
