3028793f870df9ade715db6b1b61bb7011fa2ef5
Thomas Moseler <tmoseler@gmx.de>
2016-02-23 15:07:25 +0100

    Aufräumen Filter Spieler Formular

--- sites/all/modules/custom/sbl_form_helper/js/after_ajax.js
+++
@@ -1,8 +0,0 @@
-(function($, Drupal)
-{
-	// Our function name is prototyped as part of the Drupal.ajax namespace, adding to the commands:
-	Drupal.ajax.prototype.commands.reload_chosen = function(ajax, response, status)
-	{
-		alert('Hello');
-	};
-}(jQuery, Drupal));
