ac4c8e77a90d0307c5ab90ba05059424e0dd8aa8
marc.lang <marc.lang@gobsy.de>
2016-02-25 19:17:26 +0100

    Test XML

--- sites/all/themes/seven_sbl/php/sbl_blitztabelle.php
+++ sites/all/themes/seven_sbl/php/sbl_blitztabelle.php
@@ -50,20 +50,27 @@ border-bottom: 1px dotted #555;
 
 </tr>
 <?php
+/*
 $xml=simplexml_load_file("http://www.schachbundesliga.de/tabelle.xml") or die("Fehler: Feed konnte nicht geladen werden");
 print_r($xml);
 for($i=0;$i<=15;$i++) {
 $platz=$xml->platz[$i]->rng;
 $team=$xml->platz[$i]->team;
+*/
+$xml=simplexml_load_file("http://www.schachbundesliga.de/tabelle.xml");
+echo $xml->getName() . "<br>";
+
+foreach($xml->children() as $child)
+  {
+  echo $child->getName() . ": " . $child . "<br>";
+  }
 ?>
 <tr>
 <td><?=$platz?>.</td>
 <td><?=$team?></td>
 </tr>
 
-<?php
-}
-?>
+
 
 </table>
 </div>
