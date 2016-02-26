ab8be740b5b606f519f9f42089e3f5a693fd80ae
marc.lang <marc.lang@gobsy.de>
2016-02-26 11:09:44 +0100

    XML

--- sites/all/themes/seven_sbl/php/sbl_blitztabelle.php
+++ sites/all/themes/seven_sbl/php/sbl_blitztabelle.php
@@ -39,12 +39,17 @@ border-bottom: 1px dotted #555;
 <body>
 
 <div class="panel panel-default">
-<div class="panel-heading"> <span class="glyphicon glyphicon-list-alt"></span><b>Schachbundesliga Tabelle</b></div>
 <div class="panel-body">
 <div class="row">
 <div class="col-xs-12">
 <table class="views-table cols-4 footable">
 <thead>
+<tr>
+<td colspan="4" class="panel-heading">
+<span class="glyphicon glyphicon-list-alt"></span><b>Schachbundesliga Tabelle</b>
+</td>
+</tr>
+
 <tr>
 <th class="views-field views-field-counter">Pl.</th>
 <th class="views-field views-field-php-2">Team</th>
@@ -69,7 +74,7 @@ $rang=$xml->channel->item[$i]->title;
 $linktext=$xml->channel->item[$i]->link;
 $mannschaft=$xml->channel->item[$i]->description;
 $mapu=$xml->channel->item[$i]->guid;
-$brepu=$xml->$xml->channel->item[$i]->pubDate;
+$brepu=$xml->channel->item[$i]->pubDate;
 ?>
 <tr<?php echo bcmod($i,2)==0?" class='odd'":" class='even'";?>>
 <td class="views-field views-field-counter"><?=$rang?></td>
@@ -80,13 +85,12 @@ $brepu=$xml->$xml->channel->item[$i]->pubDate;
 <?php
 }
 ?>
-
-
+<tr><td colspan="4" class="panel-footer">
+<tr><td colspan="4" class="panel-footer"><img src="images/logo.png" style="margin-left:5px;" /></td></tr>
 </table>
 </div>
 </div>
 </div>
-<div class="panel-footer"><img src="images/logo.png" style="margin-left:5px;" />
-</div>
+
 
 
