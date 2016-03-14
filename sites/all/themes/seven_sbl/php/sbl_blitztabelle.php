8b52e82ac892fc8f576cac7b32534a37ccd5e16b
marc.lang <marc.lang@gobsy.de>
2016-03-14 11:48:32 +0100

    paar Updates

--- sites/all/themes/seven_sbl/php/sbl_blitztabelle.php
+++ sites/all/themes/seven_sbl/php/sbl_blitztabelle.php
@@ -1,8 +1,18 @@
 <?php
 header('content-type:application/x-javascript;charset=utf-8');
+$bgcolor=strlen($_GET["bgcolor"])>=3?str_replace("#","",$_GET["bgcolor"]):"f18000";
+$linkcolor=strlen($_GET["linkcolor"])>=3?str_replace("#","",$_GET["linkcolor"]):"f18000";
+$headingcolor=strlen($_GET["headingcolor"])>=3?str_replace("#","",$_GET["headingcolor"]):"ffffff";
+$bordercolor=strlen($_GET["bordercolor"])>=3?str_replace("#","",$_GET["bordercolor"]):"f18000";
 ?>
 document.write('<meta charset="utf-8" />');
 <?php
+/*
+bgcolor: Die Hintergrundfarbe des Headers und Footers der Tabelle.
+linkcolor: Die Farbe der Links, also der Vereinsnamen und des Links "Zur ausführlichen Tabelle"
+headingcolor: Die Farbe der Tabellenüberschrift. Diese ist aktuell weiß, aber wenn Sie für bgcolor einen hellen Farbton wählen, müssen Sie die Tabellenüberschrift entsprechend dunkler machen, da man sie sonst nicht mehr sieht.
+*/
+
 function transform_link($linktext,$saison) {
 	$linktext=str_replace("schachbundesliga.de/","schachbundesliga.de/verein/",$linktext);
 	$linktext.="/".$saison;
@@ -11,8 +21,8 @@ function transform_link($linktext,$saison) {
 ?>
 document.writeln('<link rel="stylesheet" href="http://schachbundesliga.de/sites/all/themes/seven_sbl/php/css/custom_tabelle.css"/>');
 
-document.write('<div class="panel panel-default"><div class="panel-body-table"><table class="views-table cols-3 footable">');
-document.write('<thead><tr><th colspan="3" class="panel-heading-table">Schachbundesliga Tabelle</th></tr>');
+document.write('<div class="panel panel-default" style="border:1px solid #<?=$bordercolor?> !important;"><div class="panel-body-table"><table class="views-table cols-3 footable">');
+document.write('<thead><tr><th colspan="3" class="panel-heading-table" style="background-color:#<?=$bgcolor?>;color:#<?=$headingcolor?>">Schachbundesliga Tabelle</th></tr>');
 document.write('<tr><th class="views-field views-field-counter">Pl.</th><th class="views-field views-field-php-2">Team</th><th class="views-field views-field-php-3">MP</th></tr>');
 document.write('</thead>');
 
@@ -28,10 +38,10 @@ $mannschaft=$xml->channel->item[$i]->description;
 $mapu=$xml->channel->item[$i]->guid;
 #$brepu=$xml->channel->item[$i]->pubDate;
 ?>
-document.write('<tr<?php echo bcmod($i,2)==0?" class=\"odd\"":" class=\"even\"";?>><td class="views-field views-field-counter"><?=$rang?></td><td class="team_text_left"><a href="<?php transform_link($linktext,$aktuelle_saison); ?>" class="link_to_team"><?=$mannschaft?></a></td>');
+document.write('<tr<?php echo bcmod($i,2)==0?" class=\"odd\"":" class=\"even\"";?>><td class="views-field views-field-counter"><?=$rang?></td><td class="team_text_left"><a href="<?php transform_link($linktext,$aktuelle_saison); ?>" class="link_to_team" style="color:#<?=$linkcolor?> !important;"><?=$mannschaft?></a></td>');
 document.write('<td class="views-field views-field-php-2"><?=$mapu?></td>');
 
 <?php
 }
 ?>
-document.write('<tr><td colspan="3" class="panel-footer-table"><img src="http://schachbundesliga.de/sites/all/themes/seven_sbl/php/images/logo.png" style="margin-left:5px;" /></td></tr></table><br /><div style="text-align:right"><img src="http://schachbundesliga.de/sites/all/themes/seven_sbl/php/images/arrow.png"> <a href="http://www.schachbundesliga.de/tabelle/<?=$aktuelle_saison?>" class="orange">Zur ausführlichen Tabelle</a></div></div></div>');
+document.write('<tr><td colspan="3" class="panel-footer-table" style="background-color:#<?=$bgcolor?> !important;"><a href="http://www.schachbundesliga.de/tabelle/<?=$aktuelle_saison?>"><img src="http://schachbundesliga.de/sites/all/themes/seven_sbl/php/images/logo.png" style="margin-left:5px;" /></a></td></tr></table><br /><div style="text-align:right"><a href="http://www.schachbundesliga.de/tabelle/<?=$aktuelle_saison?>" class="link_to_team" style="color:#<?=$linkcolor?> !important;font-size:12px;">>> Zur ausführlichen Tabelle</a></div></div></div>');
---
+++ sites/all/themes/seven_sbl/php/sbl_dwz-rechner.php
@@ -0,0 +1,61 @@
+<?php
+error_reporting(0);
+$designs=Array("sbl","red","dark","green","blue");
+$_GET["design"]=strtolower($_GET["design"]);
+if(in_array($_GET["design"],$designs)) $helpicon="_".$_GET["design"];
+else $helpicon="_sbl";
+?>
+<html lang="de" dir="ltr" version="HTML+RDFa 1.1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+  <head>
+
+<link rel="stylesheet" type="text/css" href="css/tooltipster.css" />
+<script type="text/javascript" src="http://code.jquery.com/jquery-1.7.0.min.js"></script>
+<script type="text/javascript" src="js/jquery.tooltipster.min.js"></script>
+
+<link rel="stylesheet" type="text/css" href="css/dwz_rechner_css.php?design=<?=$_GET["design"]?>&bgcolor=<?=$_GET["bgcolor"]?>&bordercolor=<?=$_GET["bordercolor"]?>&header=<?=$_GET["header"]?>&headingcolor=<?=$_GET["headingcolor"]?>" />
+<script type="text/javascript">
+jQuery(function($) { // onDomReady
+
+    // reset handler that clears the form
+    $('form[name="sbl_dwzrechner"] input:reset').click(function () {
+        $('form[name="sbl_dwzrechner"]')
+            .find('textarea, :text, select').val('')
+
+        return false;
+    });
+
+});
+</script>
+
+<script type="text/javascript">
+ $(document).ready(function() {
+            $('.tooltip').tooltipster();
+        });
+</script>
+
+</head>
+
+<body>
+
+<form action="http://schachbundesliga.de/dwz-rechner#auswertung" method="POST" target="_blank" name="sbl_dwzrechner">
+
+<div class="header">DWZ-Rechner</div>
+<div class="dwzrechner">
+
+<label for="alte_dwz"><strong>Bisherige DWZ:</strong><img class="tooltip" src="images/help<?=$helpicon?>.png" title="Ihre aktuelle DWZ. Wenn Sie noch keine haben, geben Sie hier bitte einfach 0 ein" /></label><input type="text" name="alte_dwz" value="<?php echo isset($_POST["alte_dwz"])?$_POST["alte_dwz"]:""; ?>" class="elo" />
+<hr /><label for="geburtsjahr"><strong>Alter:</strong></label><select name="geburtsjahr" size="1">
+<option value="1990">älter als 25</option>
+<option value="<?php echo (date("Y",time()) - 22); ?>">21-25</option>
+<option value="<?php echo (date("Y",time()) - 10); ?>">20 oder jünger</option>
+</select>
+<hr /><label for="schnellberechnung_gegnerschnitt"><strong>Gegnerschnitt:</strong><img class="tooltip" src="images/help<?=$helpicon?>.png" title="Der DWZ-Schnitt aller Ihrer Gegner. Partien gegen Spieler ohne DWZ können nicht gewertet werden" /></label><input type="text" name="schnellberechnung_gegnerschnitt" value="<?=$_POST["schnellberechnung_gegnerschnitt"]?>" class="elo" /><br />
+<label for="schnellberechnung_punkte"><strong>Erzielte Punkte:</strong><img class="tooltip" src="images/help<?=$helpicon?>.png" title="Die erzielten Punkte und die Anzahl der Partien gegen Gegner mit DWZ. Partien gegen Spieler ohne DWZ können nicht gewertet werden. Beispiel: 3,0/4" /></label><input type="text" name="schnellberechnung_punkte" value="<?=$_POST["schnellberechnung_punkte"]?>" class="punkte" /> / <input type="text" name="schnellberechnung_partien" value="<?=$_POST["schnellberechnung_partien"]?>" class="punkte" />
+<hr /><div style="text-align:center;"><input type="submit" class="submit" name="berechnen" value="Berechnen!">&nbsp;&nbsp;<input type="reset" class="submit" name="reset" value="L&ouml;schen"></div>
+<hr />
+<div class="footer"><a href="http://www.schachbundesliga.de/dwz-rechner" target="_blank"><img src='images/logo_powered.png' class="tooltip" title="Zum ausführlichen DWZ-Rechner auf schachbundesliga.de"></div></div>
+</form>
+</body>
+</html>
+
+
