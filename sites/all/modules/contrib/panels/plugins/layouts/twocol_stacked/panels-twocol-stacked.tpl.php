c9bd75dbbe69204cbd02f1713ecea61271f1d636
U-BFC-PC\BFC <C:\Users\BFC\AppData\Roaming\The Bat!\Mail>
2016-08-18 15:46:09 +0200

    Panels Sicherheitsupdate

--- sites/all/modules/contrib/panels/plugins/layouts/twocol_stacked/panels-twocol-stacked.tpl.php
+++ sites/all/modules/contrib/panels/plugins/layouts/twocol_stacked/panels-twocol-stacked.tpl.php
@@ -30,7 +30,6 @@
     <div class="panel-col-last panel-panel">
       <div class="inside"><?php print $content['right']; ?></div>
     </div>
-	<div class="clearboth"></div>
   </div>
 
   <?php if ($content['bottom']): ?>
