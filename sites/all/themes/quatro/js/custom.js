e47076634f0a610b0a12a8b36b1a2ea426336d55
Thomas Moseler <tmoseler@gmx.de>
2016-11-07 15:52:52 +0100

    In Kader-Tabelle bei den Ergebnissen pro Spieler und Spieltag leere Spalten verstecken

--- sites/all/themes/quatro/js/custom.js
+++ sites/all/themes/quatro/js/custom.js
@@ -70,6 +70,25 @@ jQuery("#container_twitter").show();
         });
     }
   };
+  Drupal.behaviors.hide_empty_table_rows = {
+    attach: function () {
+      $('.page-verein .view-id-players table').each(function(a, tbl) {
+        var currentTableRows = $(tbl).find('tbody tr').length;
+        $(tbl).find('th').each(function(i) {
+            var remove = 0;
+            var currentTable = $(this).parents('table');
+
+            var tds = currentTable.find('tr td:nth-child(' + (i + 1) + ')');
+            tds.each(function(j) { if ($(this).text().trim() === '') remove++; });
+
+            if (remove == currentTableRows) {
+                $(this).hide();
+                tds.hide();
+            }
+        });
+    });
+    }
+  };
 })(jQuery);
 
 jQuery(document).ready(function() {
